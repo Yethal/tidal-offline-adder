cfc842dafd161b7fb407e5ad349312adfd37d1c5
Yethal <grzegorz.chodzicki@gmail.com>
2021-01-24 00:05:08 +0100

    Create downloader.sh

---
+++ downloader.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+launchTidal(){
+    adb shell monkey -p com.aspiro.tidal -c android.intent.category.LAUNCHER 1
+}
+getCords(){
+    adb exec-out uiautomator dump /dev/tty | awk '{gsub("UI hierchary dumped to: /dev/tty", "");print}'|xmllint -xpath "//node$1/@bounds" - | awk -F "]" '{gsub("bounds=","");gsub(""\[","");gsub(","," ");print $1}'
+}
+pressButton(){
+    xargs -r -I{} adb shell input tap {}
+}
+export -f pressButton
+
+openMyCollection(){
+    getCords "[@text="My Collection"]/preceding-sibling::node"|pressButton
+}
+openAlbums(){
+    getCords "[@text="Albums"]/preceding-sibling::node"|pressButton
+}
+export -f getCords
+
+getOptions(){
+    getCords "[@resource-id="com.aspiro.tidal:id/options"]"
+}
+export -f getOptions
+
+getAlbumNames(){
+    adb exec-out uiautomator dump /dev/tty | awk '{gsub("UI hierchary dumped to: /dev/tty", "");print}'|xmllint -xpath "//node[@resource-id="com.aspiro.tidal:id/title"]/@text" -
+}
+export -f getAlbumNames
+
+downloadAlbum() {
+    adb shell input tap "$1" "$2"
+    getCords "[@text="Download"]"|pressButton
+}
+export -f downloadAlbum
+
+scrollAlbums(){
+scrollTo="$(getCords [@text="Albums"])"
+scrollFrom="$(getOptions|tail -n1)"
+adb shell input swipe "$scrollFrom" "$scrollTo" 2500
+}
+export -f scrollAlbums
+
+#app launch
+launchTidal
+openMyCollection
+openAlbums
+while true; do
+getOptions|xargs -I {} bash -c  'downloadAlbum {}'
+scrollAlbums
+done
+
+#this loop is for debug purposes
+#for i in {1..20}
+#do
+   #getAlbumNames
+   #scrollAlbums
+#done
