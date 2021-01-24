c61512f901e06a5afbb52f84656b6910ead13f64
Yethal <grzegorz.chodzicki@gmail.com>
2021-01-24 16:48:49 +0100

    Update downloader.sh
    Cleanups

--- downloader.sh
+++ downloader.sh
@@ -2,47 +2,64 @@
 #increase this value if you keep hitting duplicates
 swipe=2500
 
+#selectors
+mycollection="//node[@text=\"My Collection\"]/preceding-sibling::node"
+albums="//node[@text='Albums']/preceding-sibling::node"
+options="//node[@resource-id=\"com.aspiro.tidal:id/options\"]"
+albumnames="//node[@resource-id='com.aspiro.tidal:id/title']/@text"
+downloadButton="//node[@text='Download']"
+albumBanner="//node[@text=\"Albums\"]"
+
 launchTidal(){
     adb shell monkey -p com.aspiro.tidal -c android.intent.category.LAUNCHER 1
 }
-getCords(){
-    adb exec-out uiautomator dump /dev/tty | awk '{gsub("UI hierchary dumped to: /dev/tty", "");print}'|xmllint --xpath "//node$1/@bounds" - | awk -F "]" '{gsub("bounds=","");gsub(""\[","");gsub(","," ");print $1}'
+export -f launchTidal
+
+getLayout(){
+    adb exec-out uiautomator dump /dev/tty | awk '{gsub("UI hierchary dumped to: /dev/tty", "");print}'
 }
+export -f getLayout
+
+getCords(){
+    getLayout|xmllint --xpath "$1/@bounds" - | awk '{gsub("[^0-9.-]"," "); print $1,$2}'
+}
+export -f getCords
+
 pressButton(){
     xargs -r -I{} adb shell input tap {}
 }
 export -f pressButton
 
 openMyCollection(){
-    getCords "[@text=\"My Collection\"]/preceding-sibling::node"|pressButton
+    getCords "$mycollection"|pressButton
 }
 export -f openMyCollection
 
 openAlbums(){
-    getCords "[@text=\"Albums\"]/preceding-sibling::node"|pressButton
+    getCords "$albums"|pressButton
 }
 export -f getCords
 
 getOptions(){
-    getCords "[@resource-id=\"com.aspiro.tidal:id/options\"]"
+    getCords "$options"
 }
 export -f getOptions
 
 getAlbumNames(){
-    adb exec-out uiautomator dump /dev/tty | awk '{gsub("UI hierchary dumped to: /dev/tty", "");print}'|xmllint --xpath "//node[@resource-id=\"com.aspiro.tidal:id/title\"]/@text" -| cut -f2 -d '=' | tr -d \"
+    getLayout|xmllint --xpath "$albumnames" -|cut -f2 -d '"'
 }
 export -f getAlbumNames
 
 downloadAlbum() {
-    adb shell input tap "$1" "$2"
-    getCords "[@text=\"Download\"]"|pressButton
+    pressButton
+    getCords "$downloadButton"|pressButton
 }
 export -f downloadAlbum
 
 scrollAlbums(){
-scrollTo="$(getCords [@text=\"Albums\"])"
-scrollFrom="$(getOptions|tail -n1)"
-adb shell input swipe "$scrollFrom" "$scrollTo" "$swipe"
+    scrollTo="$(getCords $albumBanner)"
+    scrollFrom="$(getOptions|tail -n1)"
+    adb shell input swipe "$scrollFrom" "$scrollTo" "$swipe"
 }
 export -f scrollAlbums
 
@@ -51,13 +68,14 @@ launchTidal
 openMyCollection
 openAlbums
 while true; do
-getOptions|xargs -I {} bash -c  'downloadAlbum {}'
-scrollAlbums
+    getOptions|xargs -I {} bash -c  'downloadAlbum {}'
+    scrollAlbums
 done
 
 #this loop is for debug purposes
 #for i in {1..20}
 #do
-   #getAlbumNames
-   #scrollAlbums
+#   getAlbumNames
+#   echo "========"
+#   scrollAlbums
 #done
